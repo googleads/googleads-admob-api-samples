2c6ecbe7f5e17a54c7520f60024c1d255295c8f5
Amira Badreldin <amirab@google.com>
2020-03-30 14:40:35 -0700

    Prepare for release

---
+++ java/v1/main/java/com/google/api/services/samples/admob/reports/GenerateNetworkReport.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.api.services.samples.admob.reports;
+
+import com.google.api.client.googleapis.util.Utils;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.services.admob.v1.AdMob;
+import com.google.api.services.admob.v1.model.Date;
+import com.google.api.services.admob.v1.model.DateRange;
+import com.google.api.services.admob.v1.model.GenerateNetworkReportRequest;
+import com.google.api.services.admob.v1.model.GenerateNetworkReportResponse;
+import com.google.api.services.admob.v1.model.NetworkReportSpec;
+import com.google.api.services.samples.admob.AdMobFactory;
+import com.google.api.services.samples.admob.util.DateUtils;
+import com.google.common.collect.ImmutableList;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.Clock;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.List;
+
+/** This example illustrates how to generate a network report. */
+public class GenerateNetworkReport {
+  /* ACCOUNT_NAME should follow the format "accounts/pub-XXXXXXXXXXXXXXXX"
+   * where "pub-XXXXXXXXXXXXXXXX" is your publisher ID
+   * See https://support.google.com/admob/answer/2784578
+   * for instructions on how to find your publisher ID.
+   */
+  private static final String ACCOUNT_NAME = "accounts/pub-XXXXXXXXXXXXXXXX";
+
+  public static void runExample(
+      AdMob adMob, String accountName, GenerateNetworkReportRequest request)
+      throws Exception {
+
+    // Get network report.
+    InputStream response =
+        adMob
+            .accounts()
+            .networkReport()
+            .generate(accountName, request)
+            .executeAsInputStream();
+
+    List<GenerateNetworkReportResponse> result =
+        Arrays.asList(
+            new JsonObjectParser(Utils.getDefaultJsonFactory())
+                .parseAndClose(
+                    response, StandardCharsets.UTF_8, GenerateNetworkReportResponse[].class));
+
+    // Print each record in the response stream.
+    for (GenerateNetworkReportResponse record : result) {
+      System.out.printf("%s%n", record);
+    }
+  }
+
+  public static GenerateNetworkReportRequest getNetworkReportRequest() {
+    /* AdMob API only supports the account default timezone and "America/Los_Angeles", see
+     * https://developers.google.com/admob/api/v1/reference/rest/v1/accounts.networkReport/generate
+     * for more information.
+     */
+    String timeZone = "America/Los_Angeles";
+    Clock clock = Clock.system(ZoneId.of(timeZone));
+
+    // Specify date range.
+    Date startDate = DateUtils.daysBeforeNow(clock, 30);
+    Date endDate = DateUtils.today(clock);
+    DateRange dateRange = new DateRange().setStartDate(startDate).setEndDate(endDate);
+
+    // Specify metrics.
+    ImmutableList<String> metrics = ImmutableList.of("IMPRESSIONS", "MATCH_RATE");
+
+    // Specify dimensions.
+    ImmutableList<String> dimensions = ImmutableList.of("FORMAT", "AD_UNIT");
+
+    // Create network report specification.
+    NetworkReportSpec reportSpec =
+        new NetworkReportSpec()
+            .setDateRange(dateRange)
+            .setTimeZone(timeZone)
+            .setMetrics(metrics)
+            .setDimensions(dimensions);
+
+    // Create network report request.
+    return new GenerateNetworkReportRequest().setReportSpec(reportSpec);
+  }
+
+  public static void main(String[] args) throws Exception {
+    AdMob adMob = AdMobFactory.getInstance();
+
+    GenerateNetworkReportRequest request = getNetworkReportRequest();
+
+    runExample(adMob, ACCOUNT_NAME, request);
+  }
+}
---
+++ java/v1/main/java/com/google/api/services/samples/admob/util/DateUtils.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.api.services.samples.admob.util;
+
+import com.google.api.services.admob.v1.model.Date;
+import java.time.Clock;
+import java.time.ZonedDateTime;
+
+/*
+ * Helper class to create the AdMob Reporting API Date object.
+ */
+public final class DateUtils {
+
+  private DateUtils() {}
+
+  public static Date today(Clock clock) {
+    ZonedDateTime dateTime = ZonedDateTime.now(clock);
+    return toDate(dateTime);
+  }
+
+  public static Date yesterday(Clock clock) {
+    return daysBeforeNow(clock, 1L);
+  }
+
+  public static Date daysBeforeNow(Clock clock, long days) {
+    ZonedDateTime dateTime = ZonedDateTime.now(clock).minusDays(days);
+    return toDate(dateTime);
+  }
+
+  private static Date toDate(ZonedDateTime dateTime) {
+    return new Date()
+        .setYear(dateTime.getYear())
+        .setMonth(dateTime.getMonthValue())
+        .setDay(dateTime.getDayOfMonth());
+  }
+}
