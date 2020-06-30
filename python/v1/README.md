4606d911de4de72da57a0565288dab10aba56fbf
Amira Badreldin <amirab@google.com>
2020-06-30 11:30:16 -0700

    AdMob API Python samples

---
+++ python/v1/README.md
@@ -0,0 +1,44 @@
+# AdMob API Python Samples
+
+This collection of samples written in Python provide usage examples for the AdMob API.
+
+## Prerequisites
+
+Please make sure that you have [pip](https://pip.pypa.io/en/stable/installing/) installed and Python 3.5 or higher.
+
+## Setup Authentication
+
+This API uses OAuth 2.0. See [Using OAuth 2.0 to Access Google APIs](https://developers.google.com/identity/protocols/oauth2) to learn more.
+
+To get started quickly, follow these steps.
+
+1. Visit https://console.developers.google.com to register your application.
+1. From the API Manager -> Google APIs screen, activate access to **AdMob API**.
+1. Click on **Credentials** in the left navigation menu.
+1. Click the button labeled **Create credentials** and select **OAuth Client ID**.
+1. Select **Other** as the **Application type**, then **Create**.
+1. From the Credentials page, click **Download JSON** next to the client ID you just created and save the file as `client_secrets.json` in the root directory of the samples.
+
+## Set up your environment ##
+### Via the command line ###
+
+Execute the following command in the root directory of the AdMob API Python client library samples to install the dependencies.
+
+    $ pip install --upgrade google-api-python-client
+    $ pip install --upgrade google-auth-oauthlib
+
+## Running the Examples ##
+
+Before proceeding with the following steps, make sure you are in the root directory of the AdMob API Python client library samples.
+
+1. Open an example and replace the **PUBLISHER_ID** constant with your Publisher ID.
+
+1. To run the sample, execute the following command.
+
+        $ python <sample_name>
+
+1. Complete the authorization steps in the browser.
+
+1. Enter the authorization code in the terminal surrounded by double quotes, for example, "AUTHORIZATION_CODE".
+
+1. Enjoy exploring the AdMob API!
---
+++ python/v1/generate_mediation_report.py
@@ -0,0 +1,85 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import admob_utils
+
+# Set the 'PUBLISHER_ID' which follows the format "pub-XXXXXXXXXXXXXXXX".
+# See https://support.google.com/admob/answer/2784578
+# for instructions on how to find your publisher ID.
+PUBLISHER_ID = 'pub-XXXXXXXXXXXXXXXX'
+
+
+def generate_mediation_report(service, publisher_id):
+  """Generates and prints a mediation report.
+
+  Args:
+    service: An AdMob Service Object.
+    publisher_id: An ID that identifies the publisher.
+  """
+
+  # Set date range. AdMob API only supports the account default timezone and
+  # "America/Los_Angeles", see
+  # https://developers.google.com/admob/api/v1/reference/rest/v1/accounts.networkReport/generate
+  # for more information.
+  date_range = {
+      'start_date': {'year': 2020, 'month': 1, 'day': 1},
+      'end_date': {'year': 2020, 'month': 3, 'day': 30}
+  }
+
+  # Set dimensions.
+  dimensions = ['DATE', 'APP', 'PLATFORM', 'COUNTRY']
+
+  # Set metrics.
+  metrics = ['ESTIMATED_EARNINGS', 'AD_REQUESTS', 'MATCHED_REQUESTS']
+
+  # Set sort conditions.
+  sort_conditions = {'dimension': 'DATE', 'order': 'DESCENDING'}
+
+  # Set dimension filters.
+  dimension_filters = {
+      'dimension': 'COUNTRY',
+      'matches_any': {
+          'values': ['US', 'CA']
+      }
+  }
+
+  # Create mediation report specifications.
+  report_spec = {
+      'date_range': date_range,
+      'dimensions': dimensions,
+      'metrics': metrics,
+      'sort_conditions': [sort_conditions],
+      'dimension_filters': [dimension_filters]
+  }
+
+  # Create mediation report request.
+  request = {'report_spec': report_spec}
+
+  # Execute mediation report request.
+  result = service.accounts().mediationReport().generate(
+      parent='accounts/{}'.format(publisher_id), body=request).execute()
+
+  # Display results.
+  for report_line in result:
+    print(report_line)
+  print()
+
+
+def main():
+  service = admob_utils.authenticate()
+  generate_mediation_report(service, PUBLISHER_ID)
+
+
+if __name__ == '__main__':
+  main()
